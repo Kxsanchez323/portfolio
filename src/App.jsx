5595e0d1013d6e8a12fb8e4f01277f6d98fee9de
Kevin Sanchez <Chilledsonic.ks2@gmail.com>
2024-11-24 10:51:44 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto relative'>
+     <Navbar />
+     <Hero />
+    </main>
+  )
+}
+
+export default App
